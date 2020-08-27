73a5411c2c1c7c25cfc87388241dcedf9c454e30
Sharanya Vinod <svinod@adobe.com>
2020-08-27 15:18:36 +0200

    ci: added semantic-release
    Semantic-release and configs added for enablic commit analysis and release notes generation

---
+++ .releaserc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+  ],
+  branch: 'test-commit',
+  branches: ['test-commit'],
+};
