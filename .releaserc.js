877a01e0b8f7e32de7157dce2be2eb8b1f16a0da
Sharanya Vinod <svinod@adobe.com>
2020-08-28 10:15:58 +0200

    ci: added semantic-release changelog

--- .releaserc.js
+++ .releaserc.js
@@ -1,7 +1,10 @@
 module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator"
+    "@semantic-release/release-notes-generator",
+    ["@semantic-release/changelog", {
+      "changelogFile": "CHANGELOG.md",
+    }]
   ],
   branch: 'master',
   branches: ['master', 'development']
