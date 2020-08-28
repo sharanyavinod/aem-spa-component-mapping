8466668395ab86a814eeafb6758328fa8ce2cbed
Sharanya Vinod <svinod@adobe.com>
2020-08-28 10:20:22 +0200

    ci: added steps for release and tag creation

--- .releaserc.js
+++ .releaserc.js
@@ -4,7 +4,15 @@ module.exports = {
     "@semantic-release/release-notes-generator",
     ["@semantic-release/changelog", {
       "changelogFile": "CHANGELOG.md",
-    }]
+    }],
+    ["@semantic-release/npm", {
+      npmPublish: false,
+    }],
+    ["@semantic-release/git", {
+      "assets": ["package.json", "CHANGELOG.md"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }],
+    '@semantic-release/github',
   ],
   branch: 'master',
   branches: ['master', 'development']
